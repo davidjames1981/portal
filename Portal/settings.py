512280f62583e752d74cb52b13eda509bff178db
David Bishop (Laptop) <david.james.bishop@outlook.com>
2024-09-25 22:46:20 +0100

    update

--- Portal/settings.py
+++ Portal/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 
 database_url = os.environ.get("DATABASE_URL")
 
-DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES["default"] = dj_database_url.parse("postgresql://db_messaround_user:ChQ81JOdIj0QWJyLt823vO9qaYa50lyM@dpg-crq6vb52ng1s73e3gjs0-a.frankfurt-postgres.render.com/db_messaround")
 
 #dj_database_url = os.environ.get("DATABASE_URL")
 
